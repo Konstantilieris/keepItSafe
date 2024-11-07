4899443d69dc3dd247a7840e2535fb8014189cf8
ANGELOS KONSTANTILIERIS <aggeloskonstantilieris@gmail.com>
2024-11-07 06:12:58 +0200

    another try

--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -1,12 +1,21 @@
 import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
 import { appRouter } from "@/server";
+import { prisma } from "@/server/server";
 
-const handler = (req: Request) => {
-  return fetchRequestHandler({
-    endpoint: "/api/trpc",
-    req,
-    router: appRouter,
-    createContext: () => ({}),
-  });
+const handler = async (req: Request) => {
+  try {
+    return await fetchRequestHandler({
+      endpoint: "/api/trpc",
+      req,
+      router: appRouter,
+      createContext: () => ({
+        prisma,
+      }),
+    });
+  } catch (error) {
+    console.error("Error handling request:", error);
+    return new Response("An error occurred", { status: 500 });
+  }
 };
+
 export { handler as GET, handler as POST };
